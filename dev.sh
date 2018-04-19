4156c43396e14b8cc49872311e535424b121c09b
Carl Foster <carl.foster@abx.com>
2018-04-19 15:23:46 +1000

    Get local https setup working

--- dev.sh
+++ dev.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/bash
 
-docker rm test
+docker kill test || true
+docker rm test || true
 docker run \
+  -d \
   -v "/home/carl/certs/bigbraincarl.com/fullchain1.pem:/certs/bigbraincarl.com.pem" \
   -v "/home/carl/certs/bigbraincarl.com/privkey1.pem:/certs/bigbraincarl.com.key.pem" \
   -p 80:80 \
