41917c1c260a6087955016684df9961826e3dfdd
Carl Foster <carl.foster@abx.com>
2018-04-19 16:00:44 +1000

    Make dev.sh consistent

--- dev.sh
+++ dev.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/bash
 
-docker kill test || true
-docker rm test || true
+docker kill carl-websites || true
+docker rm carl-websites || true
 docker run \
   -d \
-  -v "/home/carl/certs/bigbraincarl.com/fullchain1.pem:/certs/bigbraincarl.com.pem" \
-  -v "/home/carl/certs/bigbraincarl.com/privkey1.pem:/certs/bigbraincarl.com.key.pem" \
+  -v "~/certs/bigbraincarl.com/fullchain1.pem:/certs/bigbraincarl.com.pem" \
+  -v "~/certs/bigbraincarl.com/privkey1.pem:/certs/bigbraincarl.com.key.pem" \
   -p 80:80 \
   -p 443:443 \
-  --name test \
+  --name carl-websites \
   carl-websites
