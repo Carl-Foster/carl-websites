cdee08f6024d630323d0f65d78e0dc0ec88cf5f3
Carl Foster <carl.foster@abx.com>
2018-04-19 16:51:29 +1000

    Fix up dev.sh and picturesofcarl

--- dev.sh
+++ dev.sh
@@ -4,8 +4,10 @@ docker kill carl-websites || true
 docker rm carl-websites || true
 docker run \
   -d \
-  -v "~/certs/bigbraincarl.com/fullchain1.pem:/certs/bigbraincarl.com.pem" \
-  -v "~/certs/bigbraincarl.com/privkey1.pem:/certs/bigbraincarl.com.key.pem" \
+  -v "$HOME/certs/bigbraincarl.com/fullchain1.pem:/certs/bigbraincarl.com.pem" \
+  -v "$HOME/certs/bigbraincarl.com/privkey1.pem:/certs/bigbraincarl.com.key.pem" \
+  -v "$HOME/certs/picturesofcarl.com/fullchain1.pem:/certs/picturesofcarl.com.pem" \
+  -v "$HOME/certs/picturesofcarl.com/privkey1.pem:/certs/picturesofcarl.com.key.pem" \
   -p 80:80 \
   -p 443:443 \
   --name carl-websites \
