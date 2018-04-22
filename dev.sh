89d81209f2c23e78700c44118e17a180d74871de
Carl Foster <carl.foster@abx.com>
2018-04-23 09:31:05 +1000

    Make a joel's countdown

--- dev.sh
+++ dev.sh
@@ -8,6 +8,8 @@ docker run \
   -v "$HOME/certs/bigbraincarl.com/privkey1.pem:/certs/bigbraincarl.com.key.pem" \
   -v "$HOME/certs/picturesofcarl.com/fullchain1.pem:/certs/picturesofcarl.com.pem" \
   -v "$HOME/certs/picturesofcarl.com/privkey1.pem:/certs/picturesofcarl.com.key.pem" \
+  -v "$(pwd)/picturesofcarl/dist:/var/www/picturesofcarl" \
+  -v "$(pwd)/bigbraincarl/dist:/var/www/bigbraincarl" \
   -p 80:80 \
   -p 443:443 \
   --name carl-websites \
